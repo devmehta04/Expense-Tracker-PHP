7db413122fe10b72cedaab711e12e280ffa08286
dev <devmehta0406@gmail.com>
2023-07-19 00:42:58 +0530

    commit 6

--- db_config.php
+++ db_config.php
@@ -12,23 +12,5 @@
 		  } 
 
 
-// function register($host, $user_name, $user_password, $db_name){
-// $con=mysqli_connect($host, $user_name, $user_password, $db_name);
-
-// 		// Check connection
-// 		if (mysqli_connect_errno())
-// 		  {
-// 		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-// 		  }
-// 		  else
-// 		  {
-// 		  	echo "Connection on";
-// 		  }
-
-// return $con;
-// }
-
-// register('localhost', 'root', 'admin', 'expense_tracker');
-
 ?>
 
